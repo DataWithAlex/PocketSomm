e7c36e7b1909655fbafd60edd84dc4dc47a8bbb2
Alex Sciuto <106262604+DataWithAlex@users.noreply.github.com>
2023-08-29 18:22:14 -0400

    Added Wine Taster + Firebase Login

--- app.py
+++ app.py
@@ -3,6 +3,63 @@ import matplotlib.pyplot as plt
 import io
 from PIL import Image
 
+
+### Firebase Authentication: 
+
+import pyrebase
+
+firebase_config = {
+    "apiKey": "AIzaSyDUW2pzxpjNKe7mrda8zm3wj_hZMxoDdzI",
+    "authDomain": "pocketsomm-15bd1.firebaseapp.com",
+    "databaseURL": "https://pocketsomm-15bd1.firebaseio.com",
+    "projectId": "pocketsomm-15bd1",
+    "storageBucket": "pocketsomm-15bd1.appspot.com",
+    "messagingSenderId": "67664070012",
+    "appId": "1:67664070012:web:59593f2b095773d58910f9",
+    "measurementId": "G-XNJHXDNLK9"
+}
+
+firebase = pyrebase.initialize_app(firebase_config)
+auth = firebase.auth()
+
+def login():
+    st.sidebar.title("User Authentication")
+    
+    menu = ["Login", "Signup"]
+    choice = st.sidebar.selectbox("Menu", menu, key="selected_login")
+    
+    if choice == "Login":
+        email = st.sidebar.text_input("Email")
+        password = st.sidebar.text_input("Password", type='password')
+        if st.sidebar.button("Login"):
+            try:
+                user = auth.sign_in_with_email_and_password(email, password)
+                st.sidebar.success("Logged in as: {}".format(email))
+                return user
+            except:
+                st.sidebar.error("Invalid email/password")
+        return None
+
+    elif choice == "Signup":
+        st.subheader("Create a new account")
+        new_user_email = st.text_input("Email")
+        new_user_password = st.text_input("Password", type='password')
+        confirm_password = st.text_input("Confirm password", type='password')
+        
+        if st.button("Signup"):
+            if new_user_password == confirm_password:
+                try:
+                    auth.create_user_with_email_and_password(new_user_email, new_user_password)
+                    st.success("Account created successfully!")
+                    user = auth.sign_in_with_email_and_password(new_user_email, new_user_password)
+                    st.sidebar.success("Logged in as: {}".format(new_user_email))
+                    return user
+                except:
+                    st.error("Error creating account")
+            else:
+                st.error("Passwords do not match")
+
+
 def create_infographic(selected_wine, appearance, clarity, intensity, selected_aromas, taste_intensity, overall_impressions, rating):
     fig, ax = plt.subplots(figsize=(6, 6))
 
@@ -41,10 +98,18 @@ def create_infographic(selected_wine, appearance, clarity, intensity, selected_a
 
 def main():
     # Initialize the state
+    user = login()
+    if user:
+        st.title(f"Welcome!")
+
+    firebase = pyrebase.initialize_app(firebase_config)
+    auth = firebase.auth()
+
     if 'start_tasting' not in st.session_state:
         st.session_state.start_tasting = False
     
-    st.title("Wine Tasting App")
+    st.title("PocketSomm")
+    # st.image('images/logo.jpg', use_column_width=True, width=10)
     st.write("Welcome to our wine tasting experience. Let's explore the world of wines!")
     
     # Start tasting button
@@ -53,21 +118,25 @@ def main():
 
     if st.session_state.start_tasting:
         wine_options = ["Red", "White", "Rosé", "Sparkling"]
-        selected_wine = st.selectbox("Choose a type of wine:", wine_options)
+        selected_wine = st.selectbox("Choose a type of wine:", wine_options, key = "wine_options_key")
 
         # Visual Examination
-        st.subheader("Visual Examination")
+        st.subheader("Look")
         appearance = st.text_input("Describe the wine's appearance:")
-        clarity = st.radio("Select the clarity:", ["Clear", "Cloudy", "Opaque"])
-        intensity = st.slider("Intensity:", 0, 10)
+        #clarity = st.radio("Select the clarity:", ["Clear", "Cloudy", "Opaque"])
+        intensity_options = ["Pale", "Medium", "Deep"]
+        intensity = st.selectbox("Intensity:", intensity_options, key = "intensity_options_key") # Wine Folly
+
+        hue_options = ["Purple", "Ruby", "Garnet", "Tawny", "Brown", "Straw", "Yellow", "Gold", "Amber", "Brown", "Pink", "Salmon", "Copper"]
+        hue = st.selectbox("Choose the Hue of the Wine:", hue_options, key = "hue_options_key")
 
         # Olfactory Examination
-        st.subheader("Olfactory Examination")
+        st.subheader("Olfactory Profile")
         aroma_options = ["Fruity", "Floral", "Spicy", "Woody", "Earthy", "Citrus"]
         selected_aromas = st.multiselect("Select the wine's aromas:", aroma_options)
 
         # Gustatory Examination
-        st.subheader("Gustatory Examination")
+        st.subheader("Taste Profile")
         tastes = ["Sweet", "Sour", "Salty", "Bitter"]
         taste_intensity = {taste: st.slider(taste, 0, 10) for taste in tastes}
 
